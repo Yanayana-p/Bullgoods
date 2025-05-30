e3227a87ee7e829f1c72132f886ba3f70296e12f
codexnii0 <caynn03@gmail.com>
2025-05-30 11:40:47 +0800

    updates

--- back-end/routes/authRoutes.js
+++ back-end/routes/authRoutes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const { signupUser,loginUser } = require('../controllers/authController');
 
 router.post('/login', loginUser); // POST /api/auth/signup
-
 router.post('/signup', signupUser); // POST /api/auth/login
 
 module.exports = router;
